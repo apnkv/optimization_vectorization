0aa9cec9d1be327db8022f8debb739b5c6578233
Alexey Pankov <alexpnkv@gmail.com>
2020-03-01 17:43:10 +0300

    Transform pipelines, patch rendering from lines using SyntheticPatch

--- utils.py
+++ utils.py
@@ -2,18 +2,26 @@ import numpy as np
 import tempfile
 import cairo
 import PIL
+import abc
 import os
+
 from PIL import Image, ImageOps
-from typing import Tuple
 
 
-class Synthetic:
+class SyntheticPatch:
     def __init__(self, max_x=64, max_y=64, border=4, default_line_rgba=(1, 1, 1, 1)):
         self._max_x = max_x
         self._max_y = max_y
         self._border = border
         self._default_line_rgba = default_line_rgba
         self._setup_context()
+
+    @staticmethod
+    def from_lines(lines, **init_kwargs):
+        synthetic = SyntheticPatch(**init_kwargs)
+        for line in lines:
+            synthetic.line((line[0], line[2]), (line[1], line[3]), line[4])
+        return synthetic
 
     def line(self, xs, ys, width=1, source_rgba=None,
              line_join=cairo.LINE_JOIN_MITER, line_cap=cairo.LINE_CAP_BUTT):
@@ -47,7 +55,9 @@ class Synthetic:
 
         return self.line(xs, ys, width, **line_kwargs)
 
-    def random_lines(self, n_lines):
+    def random_lines(self, n_lines, random_seed=None):
+        np.random.seed(random_seed)
+
         for _ in range(n_lines):
             self.random_line()
 
@@ -100,3 +110,61 @@ class Synthetic:
 
     def write_to_png(self, filename):
         self._surface.write_to_png(filename)
+
+
+class LinePerturbation(abc.ABC):
+    def __init__(self):
+        pass
+
+    @abc.abstractmethod
+    def transform(self, lines):
+        raise NotImplementedError('Base class LinePerturbation cannot be used')
+
+
+class LineRandomShift(LinePerturbation):
+    def __init__(self, endpoint_shift_range, width_shift_range, random_seed=None):
+        super(LineRandomShift, self).__init__()
+        self._endpoint_shift_range = endpoint_shift_range
+        self._width_shift_range = width_shift_range
+        np.random.seed(random_seed)
+
+    def transform(self, lines):
+        new_lines = [list(line) for line in lines]
+        for line in new_lines:
+            for coord in range(4):
+                line[coord] += np.random.randint(*self._endpoint_shift_range)
+
+            line[4] = max(line[4] + np.random.randint(*self._width_shift_range), 1)
+        return new_lines
+
+
+class LineClip(LinePerturbation):
+    def __init__(self, x_lo, x_hi, y_lo, y_hi):
+        super(LineClip, self).__init__()
+        self._x_lo = x_lo
+        self._x_hi = x_hi
+        self._y_lo = y_lo
+        self._y_hi = y_hi
+
+    def transform(self, lines):
+        new_lines = [list(line) for line in lines]
+        for line in new_lines:
+            line[0] = min(max(line[0], self._x_lo), self._x_hi)
+            line[2] = min(max(line[2], self._x_lo), self._x_hi)
+
+            line[1] = min(max(line[1], self._y_lo), self._y_hi)
+            line[3] = min(max(line[3], self._y_lo), self._y_hi)
+
+        return new_lines
+
+
+class LinePerturbationPipe(LinePerturbation):
+    def __init__(self, *steps):
+        super(LinePerturbationPipe, self).__init__()
+        self._steps = steps
+
+    def transform(self, lines):
+        new_lines = lines.copy()
+        for step in self._steps:
+            new_lines = step.transform(new_lines)
+        return new_lines
