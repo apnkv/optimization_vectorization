865ff50318b3cbe9d1e6aed0b980b2610b0c9480
Alexey Pankov <alexpnkv@gmail.com>
2020-05-23 18:31:17 +0300

    Moved reusable code from scripts to lib

--- scripts/align_intermediate_output.py
+++ scripts/align_intermediate_output.py
@@ -6,71 +6,10 @@ import pickle
 from collections import deque, namedtuple
 from concurrent.futures import ThreadPoolExecutor, wait
 
-import torch
-from geomloss import SamplesLoss
-from torch import optim
-
-from vecopt.aligner import (
-    StatefulBatchAligner,
-    init_ot_aligner,
-    make_default_loss_fn,
-    make_default_optimize_fn,
-)
-from vecopt.aligner_utils import (
-    LossComposition, perceptual_bce, strip_confidence_grads,
-    compose, coords_only_grads
-)
-from vecopt.crossing_model import CrossingRefinerFull
-from vecopt.inference import IntermediateOutputAligner
+from vecopt.inference import make_intermediate_output_aligner
 
 IntermediateSample = namedtuple('IntermediateSample', ['worker_idx', 'sample', 'filename'])
 AlignedSample = namedtuple('AlignedSample', ['sample', 'filename'])
-
-
-def load_crossing_model(path, device):
-    crossing_model = CrossingRefinerFull().to(device)
-    crossing_model.load_state_dict(torch.load(path))
-    _ = crossing_model.train(False)
-    return crossing_model
-
-
-def make_aligner(device, config, crossing_model=None):
-    if crossing_model is None:
-        crossing_model = load_crossing_model(config['crossing_model_weights'], device)
-
-    loss = LossComposition()
-    ot_loss = SamplesLoss("sinkhorn", **config['ot_loss'])
-    loss.add(make_default_loss_fn(
-        bce_schedule=(lambda state: 0.0),
-        ot_loss=ot_loss
-    ))
-    if 'perceptual_bce' in config:
-        for layer in config['perceptual_bce']:
-            loss.add(perceptual_bce(crossing_model, layer))
-
-    grad_transformer = compose(strip_confidence_grads, coords_only_grads(config['coord_only_grads']))
-
-    aligner = StatefulBatchAligner(device=device)
-    init_ot_aligner(aligner, loss_fn=loss, device=device,
-                    optimize_fn=make_default_optimize_fn(
-                        aligner,
-                        lr=config['lr'],
-                        transform_grads=grad_transformer,
-                        base_optimizer=optim.Adam,
-                    ))
-
-    return aligner
-
-
-def make_intermediate_output_aligner(device, config):
-    crossing_model = load_crossing_model(config['crossing_model_weights'], device)
-    aligner = make_aligner(device, config, crossing_model=crossing_model)
-    return IntermediateOutputAligner(aligner,
-                                     batch_size=config['batch_size'],
-                                     n_steps=config['n_steps'],
-                                     crossing_model=crossing_model,
-                                     infer_crossings=config['infer_crossings']
-                                     )
 
 
 class Worker:
@@ -132,11 +71,9 @@ def main(args):
 
     results = align_data_folder(args.input, args.n_workers, config)
 
-    if not os.path.exists(args.output):
-        os.mkdir(args.output)
-
     print('Finished. Writing files:')
 
+    os.makedirs(args.output, exist_ok=True)
     for sample in results:
         filename = os.path.join(args.output, sample.filename)
         print(filename)
